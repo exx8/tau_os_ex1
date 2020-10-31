27bf00e4be910d547ff2f40f3a52012bc2ea4086
exx8 <exx8eran@gmail.com>
2020-10-31 11:48:38 +0200

    add files

---
+++ cmake-build-debug/CMakeFiles/tau_os_ex1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tau_os_ex1.dir/main.c.o"
+  "tau_os_ex1"
+  "tau_os_ex1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tau_os_ex1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/tau_os_ex1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tau_os_ex1.
+# This may be replaced when dependencies are built.
