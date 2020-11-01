a1b72faaa55036719c3f79e8aee80edaa568e3d4
exx8 <exx8eran@gmail.com>
2020-11-02 00:54:32 +0200

    .

--- pt.c
+++ pt.c
@@ -32,18 +32,17 @@ void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn) {
         move_page(pt, vpn, ppn);
 }
 
-uint64_t get_level(uint64_t ppn,short level)
-{
-     int keep_only=511;
-     const int level_size=9;
-    keep_only<<=level*level_size;
-    return ppn&&keep_only;
+uint64_t get_level(uint64_t ppn, short level) {
+    int keep_only = 511;
+    const int level_size = 9;
+    keep_only <<= level * level_size;
+    return ppn && keep_only;
 
 }
+
 uint64_t page_table_query(uint64_t pt, uint64_t vpn) {
     uint64_t *virt_address = get_virt_address(pt);
-    for(short i=0;i<NLEVELS;i++)
-    {
+    for (short i = 0; i < NLEVELS; i++) {
 
     }
 
