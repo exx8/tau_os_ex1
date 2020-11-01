ff99f9d3fa4a69cf12f6af5e3dc7ff086a8c4d21
exx8 <exx8eran@gmail.com>
2020-11-02 01:02:27 +0200

    .

--- pt.c
+++ pt.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include "os.h"
 
 #define NLEVELS 5
@@ -32,17 +33,28 @@ void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn) {
         move_page(pt, vpn, ppn);
 }
 
-uint64_t get_level(uint64_t ppn, short level) {
+uint64_t get_level(uint64_t vpn, short level) {
     int keep_only = 511;
     const int level_size = 9;
     keep_only <<= level * level_size;
     return ppn && keep_only;
 
 }
-
+bool valid_is_1(uint64_t entry)
+{
+    return entry&&1;
+}
+bool invalid(uint64_t entry)
+{
+    return !valid_is_1(entry);
+}
 uint64_t page_table_query(uint64_t pt, uint64_t vpn) {
     uint64_t *virt_address = get_virt_address(pt);
     for (short i = 0; i < NLEVELS; i++) {
+        int level_value=get_level(virt_address,i);
+        if(invalid(virt_address[level_value]))
+            return NO_MAPPING;
+        //todo continue if adress IS valid
 
     }
 
